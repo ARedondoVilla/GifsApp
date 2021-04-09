757ff6f8c79d3397f67c36cdf9f2fa5de103985e
Alejandro Redondo Villa <aredondovilla@DESKTOP-NC6JR51.localdomain>
2021-04-09 20:30:12 +0200

    valor inputs

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,7 +8,16 @@ import { Component } from '@angular/core';
 })
 export class BusquedaComponent {
 
-  buscar() {
-    console.log('hey');
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;// Toma referencia local #txtBuscar en el html y la asigna a la variable txtBuscar
+  // asegura de que txtBuscar no sera null, evitando el error de que da TypeScript
+  buscar( textoBuscado: string) {
+    
+    let valor = this.txtBuscar.nativeElement.value;
+    // Se le da el valor generico HTMLInputElement para que aparezcan en el autocompletado todas las claves del native element
+    console.log(valor);
+    
+    
   }
+
+  // Esto genera en consola el elemento native element de tipo ElementRef
 }
